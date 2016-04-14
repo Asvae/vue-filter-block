11f0b5d93008587c0915bacb7ea1874b7eb2aac8
Asvae <ontrew@gmail.com>
2016-04-14 20:14:21 +0300

    Minor

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-var components = require('./components/components')
-
 module.exports = {
     mixins: {
         child: require('./mixins/child'),
         parent: require('./mixins/parent'),
     },
-    components,
+    components: require('./components/components'),
 }
-
