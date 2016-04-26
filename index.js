da583d283e0bc0f7da73c6b2ee96a2ddfe5e9606
Asvae <ontrew@gmail.com>
2016-04-26 18:46:11 +0300

    Add default export with components.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ import _components from './components/components'
 import _child from './mixins/child'
 import _parent from './mixins/parent'
 
+export default {
+    components: _components
+}
+
 export var childMixin = _child
 export var parentMixin = _parent
 export var components = _components
